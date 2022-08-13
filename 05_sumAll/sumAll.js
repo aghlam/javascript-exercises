79f89e89b41c86ae68fb003e37cf974781987d40
Alan Lam <agh.lam@hotmail.com>
2022-08-13 19:37:09 +1000

    Exercise 5 complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,27 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+
+    let start;
+    let end;
+    let sum = 0;
+
+    if (num1 > num2) {
+        end = num1;
+        start = num2;
+    } else {
+        end = num2;
+        start = num1;
+    }
+
+    if (start < 0 || typeof(end) != 'number' || typeof(start) != 'number') {
+        return 'ERROR';
+    }
+
+    while (start <= end) {
+        sum += start;
+        start++;
+    }
+
+    return sum;
 
 };
 
